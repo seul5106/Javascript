b3f8fd2a6bba772461b4243376b5c3b2412ac813
NeoJHS <seul5106@gmail.com>
2021-11-04 17:26:44 +0900

    .

--- JSON/example/ex4.js
+++ JSON/example/ex4.js
@@ -2,6 +2,7 @@ covid19 = [
     {'0125': 426}, {'0126': 343}, {'0127': 547}, {'0128': 490}, 
     {'0129': 460}, {'0130': 443}, {'0131': 338}, {'0201': 299}
 ]
+
 let sum = 0;
 let avg = 0;
 for(let i = 0; i < covid19.length; i++){
@@ -13,12 +14,16 @@ for(let i = 0; i < covid19.length; i++){
 console.log("누적 확진자 수: ", sum);
 console.log("평균 확진자 수: ", avg);
 
+/**
 let sum = 0;
 for(const j of covid19){
+    
     for(const k in j) {
         console.log(j[k]);
         sum += j[k];
     }
 }
+
 console.log("누적 확진자 수: ", sum);
 console.log("평균 확진자 수: ", sum/covid19.length);
+**/
